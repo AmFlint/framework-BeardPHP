a9cebfdec80a2d2f9046d75c5f58b2297394e1ca
AmFlint <amasselot@tradelab.fr>
2017-08-16 18:52:05 +0200

    testing on default controller

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,15 +3,10 @@
 namespace Controller;
 
 use Helper\Controller;
-use Helper\QueryBuilder;
+use Model\Cluster;
 
 class DefaultController extends Controller
 {
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
     public function fooAction()
     {
@@ -20,22 +15,12 @@ class DefaultController extends Controller
 
     public function chickAction()
     {
-        echo "wouw";
+        $cluster = Cluster::findOne(1);
+        $cluster->user;
     }
 
     public function testAction($id, $test)
     {
-//       echo  self::$twig->render(
-//           "home.html.twig",
-//           [
-//               "prenom" => $test,
-//               "age"    => $id
-//           ]
-//       );
-        $qb = new QueryBuilder();
-//        $qb->updateColumns(array('nom', 'degats'))->values(array('mate', 2))->where('id', '2')->table('personnages')->update();
-//        $qb->table('personnages')->where('id', 3)->delete();
-        $test = $qb->table('cours')->select(array("cours.name", "tags.name"), array('', 'tag'))->join('tags', 'inner')->on('cours.tag_id', 'tags.id')->where('tags.name', 'medicament')->get();
-        var_dump($test);
+
     }
 }
