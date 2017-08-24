72b6a2f069bccb8528ee239555204ca98b8991b7
AmFlint <amasselot@tradelab.fr>
2017-08-24 17:31:49 +0200

    try catch exceptions index + moved view 404

--- public/index.php
+++ public/index.php
@@ -3,12 +3,17 @@
 //var_dump($_SERVER['REQUEST_URI']);
 
 require '../vendor/autoload.php';
+try {
+    $route = new \Helper\Router($_SERVER['REQUEST_URI']);
+    $route::$url = trim($route::$url, '/');
+    if (strpos($route::$url, '?'))
+    {
+        $route::$url = substr($route::$url, 0, strpos($route::$url, '?'));
+    }
+    $route->init();
 
-$route = new \Helper\Router($_SERVER['REQUEST_URI']);
-$route::$url = trim($route::$url, '/');
-$route::$url = substr($route::$url, 0, strpos($route::$url, "?"));
-
-$route->init();
-
-$currentRoute = $route->getRoute();
-$route::generateController($currentRoute);
+    $currentRoute = $route->getRoute();
+    $route::generateController($currentRoute);
+} catch (\Exception $e) {
+    dd($e->getMessage());
+}
